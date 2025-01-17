2d8fb6cc8f48c1023026d6593f1deae143e842ac
Andrei Zeucianu <andrei.zeucianu@gmail.com>
2025-01-17 08:33:47 +0100

    Added main namespace and class

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Gtk;
 
-var application = Gtk.Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
-application.OnActivate += (sender, args) =>
-{
-    var window = Gtk.ApplicationWindow.New((Gtk.Application) sender);
-    window.Title = "Gtk4 Window";
-    window.SetDefaultSize(300, 300);
-    window.Show();
-};
-return application.RunWithSynchronizationContext(null);
+namespace TeXSharp {
+    class Program {
+        public static int Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+
+            var application = Gtk.Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
+            application.OnActivate += (sender, args) =>
+            {
+                var window = Gtk.ApplicationWindow.New((Gtk.Application) sender);
+                window.Title = "Gtk4 Window";
+                window.SetDefaultSize(300, 300);
+                window.Show();
+            };
+            return application.RunWithSynchronizationContext(null);
+        }
+    }
+}
+
