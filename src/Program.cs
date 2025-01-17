73e0a605cad9e8c900e90cf8d6f62eb5fb0a3ad7
Andrei Zeucianu <andrei.zeucianu@gmail.com>
2025-01-17 09:19:30 +0100

    Commented Hello World, and optimisations

--- src/Program.cs
+++ src/Program.cs
@@ -4,14 +4,13 @@ using Gtk;
 namespace TeXSharp {
     class Program {
         public static int Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            // Console.WriteLine("Hello, World!");
 
             var application = Gtk.Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
-            application.OnActivate += (sender, args) =>
-            {
+            application.OnActivate += (sender, args) => {
                 var window = Gtk.ApplicationWindow.New((Gtk.Application) sender);
                 window.Title = "Gtk4 Window";
-                window.SetDefaultSize(300, 300);
+                window.SetDefaultSize(800, 600);
                 window.Show();
             };
             return application.RunWithSynchronizationContext(null);
