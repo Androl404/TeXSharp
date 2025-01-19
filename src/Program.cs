1f2de1ac486ff4a08ae388267b2cf859f39cfdf3
Johann <johann.plasse@gmail.com>
2025-01-19 15:26:07 +0100

    Moved from using Gtk.Image widget to more modern Gtk.Picture widget
    This fix allows the images used to render the PDF to nicely fill the
    space available horizontally and avoid weird gaps (useless white
    spaces). While still keeping the scroll functionnality

--- src/Program.cs
+++ src/Program.cs
@@ -3,48 +3,51 @@ using Gtk;
 using Gio;
 
 class Program {
-    public static int Main(string[] args) {
-        var application = Gtk.Application.New("com.github.TeXSharp", Gio.ApplicationFlags.FlagsNone);
-        application.OnActivate += (sender, args) => {
-            // Create main Window
-            var window = new Window($"TeXSharp - {Globals.lan.ServeTrad("modern_latex_editor")}", 800, 600, sender);
-            window.SetHeaderBar();
+  public static int Main(string[] args) {
+    var application = Gtk.Application.New("com.github.TeXSharp",
+                                          Gio.ApplicationFlags.FlagsNone);
+    application.OnActivate += (sender, args) => {
+      // Create main Window
+      var window = new Window(
+          $"TeXSharp - {Globals.lan.ServeTrad("modern_latex_editor")}", 800,
+          600, sender);
+      window.SetHeaderBar();
 
-            var text_editor = window.MakeTextEditor();
-            var PDF_viewer = window.MakePDFViewer();
+      var text_editor = window.MakeTextEditor();
+      var PDF_viewer = window.MakePDFViewer();
 
-            // Create some other elements for example
-            var button1 = Gtk.Button.New();
-            button1.Label = Globals.lan.ServeTrad("save");
-            var button2 = Gtk.Button.New();
-            button2.Label = Globals.lan.ServeTrad("load");
-            var label = Gtk.Label.New(Globals.lan.ServeTrad("editor"));
+      // Create some other elements for example
+      var button1 = Gtk.Button.New();
+      button1.Label = Globals.lan.ServeTrad("save");
+      var button2 = Gtk.Button.New();
+      button2.Label = Globals.lan.ServeTrad("load");
+      var label = Gtk.Label.New(Globals.lan.ServeTrad("editor"));
 
-            // Create Grid
-            var grid = Gtk.Grid.New();
-            grid.SetHexpand(true);
-            grid.SetVexpand(true);
+      // Create Grid
+      var grid = Gtk.Grid.New();
+      grid.SetHexpand(true);
+      grid.SetVexpand(true);
 
-            // Attach widgets to the grid
-            // Syntax: grid.Attach(widget, column, row, width, height)
-            grid.Attach(label, 0, 0, 2, 1); // Spans 2 columns in the first row
-            grid.Attach(button1, 0, 1, 1, 1); // First column, second row
-            grid.Attach(button2, 1, 1, 1, 1); // Second column, second row
-            grid.Attach(text_editor, 0, 2, 2, 1); // Spans 2 columns in the third row
-            grid.Attach(PDF_viewer, 2, 2, 1, 1); // Spans 2 columns in the third row
+      // Attach widgets to the grid
+      // Syntax: grid.Attach(widget, column, row, width, height)
+      grid.Attach(label, 0, 0, 2, 1);       // Spans 2 columns in the first row
+      grid.Attach(button1, 0, 1, 1, 1);     // First column, second row
+      grid.Attach(button2, 1, 1, 1, 1);     // Second column, second row
+      grid.Attach(text_editor, 0, 2, 2, 1); // Spans 2 columns in the third row
+      grid.Attach(PDF_viewer, 2, 2, 3, 1);  // Spans 2 columns in the third row
 
-            // Optional: Add some spacing between elements
-            grid.RowSpacing = 10;
-            grid.ColumnSpacing = 10;
-            grid.SetMarginStart(15);
-            grid.SetMarginTop(15);
-            grid.SetMarginBottom(15);
-            // grid.SetMarginEnd(15);
+      // Optional: Add some spacing between elements
+      grid.RowSpacing = 10;
+      grid.ColumnSpacing = 10;
+      grid.SetMarginStart(15);
+      grid.SetMarginTop(15);
+      grid.SetMarginBottom(15);
+      // grid.SetMarginEnd(15);
 
-            // Set the grid as the window's child
-            window._Window.SetChild(grid);
-        };
+      // Set the grid as the window's child
+      window._Window.SetChild(grid);
+    };
 
-        return application.RunWithSynchronizationContext(null);
-    }
+    return application.RunWithSynchronizationContext(null);
+  }
 }
