56b6f8c06fe7635478c27c26558540d9604c26b3
Andrei Zeucianu <benjaminpotron@gmail.com>
2025-01-31 10:25:57 +0100

    Refactored buttonbar API again, with some simplifications

--- src/ButtonBar.cs
+++ src/ButtonBar.cs
@@ -19,19 +19,17 @@ class ButtonBar {
         var button_icon = Gtk.Image.NewFromGicon(icon); // We create an image with an icon
         // The names of the available icons can be found with `gtk4-icon-browser`, or in /usr/share/icons/
         button.SetChild(button_icon); // We set the icon as child of the button (the child will be contained in the button)
-        button.OnClicked += (sender, args) => {
-            func(sender, args);
-        };
+        button.OnClicked += (sender, args) => { func(sender, args); };
+        button.OnActivate += (sender, args) => { func(sender, args); };
         this.button_list.Add(label, button);
         this.box.Append(button);
     }
 
-    public void AddActivatedEvent(string label, Func<object?, EventArgs, System.Threading.Tasks.Task> func) {
-        if (!this.button_list.ContainsKey(label)) throw new System.FieldAccessException("The key does not exists in the dictionnary !");
-        this.button_list[label].OnActivate += (sender, args) => {
-            func(sender, args);
-        };
-    }
+    // public void AddActivatedEvent(string label, Func<object?, EventArgs, System.Threading.Tasks.Task> func) {
+    //     if (!this.button_list.ContainsKey(label))
+    //         throw new System.FieldAccessException("The key does not exists in the dictionnary !");
+    //     this.button_list[label].OnActivate += (sender, args) => { func(sender, args); };
+    // }
 
     // Manuals getter
     public Gtk.Box GetBox() {
